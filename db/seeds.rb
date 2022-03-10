482f9b43d38b148ec88a98ef2820440ab6fd4702
Brandon Lin <bwlin93@gmail.com>
2022-03-09 19:23:51 -0600

    seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Actor.create({ first_name: "Nathan", last_name: "Fillion", known_for: "Firefly" })
+Actor.create({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
+Actor.create({ first_name: "Christian", last_name: "Bale", known_for: "Batman" })
+Actor.create({ first_name: "Ian", last_name: "McKellen", known_for: "Gandolf" })
+Actor.create({ first_name: "Daniel", last_name: "Radcliff", known_for: "Harry Potter" })
