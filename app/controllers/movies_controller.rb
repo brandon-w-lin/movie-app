3eea71fc79e87d392e07faf8b777a436bd8f337d
Brandon Lin <bwlin93@gmail.com>
2022-03-16 20:05:37 -0500

    adds migrations to movie model to add director and an english flag

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,6 +12,8 @@ class MoviesController < ApplicationController
       title: params[:title],
       year: params[:year],
       plot: params[:plot],
+      director: params[:director],
+      english: params[:english],
     )
     movie.save
   end
@@ -23,6 +25,8 @@ class MoviesController < ApplicationController
     movie.title = params[:title] || movie.title
     movie.year = params[:year] || movie.year
     movie.plot = params[:plot] || movie.plot
+    movie.director = params[:director] || movie.director
+    movie.english = params[:english] || movie.english
 
     movie.save
   end
