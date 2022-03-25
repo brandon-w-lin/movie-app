77d9d6088cc7d2dd8d3625be1f5478276f4db9b7
Brandon Lin <bwlin93@gmail.com>
2022-03-24 21:38:31 -0500

    changes movie views to output the genre names instead of entire genre objects

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,10 @@ class Movie < ApplicationRecord
   has_many :actors
   has_many :movie_genres
   has_many :genres, through: :movie_genres
+
+  def genre_names
+    movie_genres.map do |movie_genre|
+      Genre.find_by(id: movie_genre.genre_id).name
+    end
+  end
 end
